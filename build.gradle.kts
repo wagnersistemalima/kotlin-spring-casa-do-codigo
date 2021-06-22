c857d19058ea43ca56724188256128ee0f2953e7
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-06-22 18:56:37 -0300

    swagger, documentacao da api

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,12 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("com.h2database:h2")
+
+	// dependencia swagger
+
+	implementation("io.springfox:springfox-swagger2:2.9.2")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
--- src/main/kotlin/br/com/wagner/casadocodigo/CasaDoCodigoApplication.kt
+++ src/main/kotlin/br/com/wagner/casadocodigo/CasaDoCodigoApplication.kt
@@ -2,9 +2,12 @@ package br.com.wagner.casadocodigo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+@EnableSwagger2
 @SpringBootApplication
 class CasaDoCodigoApplication
+
 
 fun main(args: Array<String>) {
 	runApplication<CasaDoCodigoApplication>(*args)
